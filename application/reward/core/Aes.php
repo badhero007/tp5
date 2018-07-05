d791829635b71a55df0b170c4e4e7fb642ffa834
376762626@qq.com <021271>
2018-07-05 16:10:29 +0800

    提交console.php

---
+++ application/reward/core/Aes.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: legolas
+ * Date: 2018/7/3
+ * Time: 17:37
+ */
+
+namespace app\reward\Core;
+
+
+class Aes
+{
+
+}
--- thinkphp/console.php
+++ thinkphp/console.php
@@ -15,6 +15,8 @@ namespace think;
 // 加载基础文件
 require __DIR__ . '/base.php';
 
+
+
 // 执行应用
 App::initCommon();
 Console::init();
